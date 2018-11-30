5ccca29a5f71397fdbeb8c5c8bff854447c2932e
Yash <yashmahes@gmail.com>
2018-11-30 14:48:15 +0530

    implemented the register user and login user functionality

--- mysite/User/urls.py
+++ mysite/User/urls.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 urlpatterns = [
-    path('register/', views.UserRegister),
+    path('login/', views.UserLogin.as_view()),
+    path('register/', views.UserRegister.as_view()),
 ]
--- mysite/User/views.py
+++ mysite/User/views.py
@@ -67,3 +67,73 @@ class UserRegister(GenericAPIView):
             return Response(content, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
+class UserLogin(GenericAPIView):
+   
+    serializer_class = UserLoginSerializer
+    def post(self, request):
+        """
+        To login in the system
+        ---
+        parameters:
+                - name: email
+                    description: Email of the user
+                    required: true
+                    type: string
+                    paramType: form
+                - name: password
+                    description: Password
+                    required: true
+                    type: password
+                    paramType: form
+                - name: device_type
+                    description: Device Type
+                    required: true
+                    type: string
+                    paramType: form
+                - name: device_id
+                    description: Device ID
+                    required: true
+                    type: string
+                    paramType: form
+
+        responseMessages:
+                - code: 400
+                    message: Invalid form details
+                - code: 401
+                    message: Not authenticated
+                - code: 422
+                    message: Unprocessable request
+                - code: 500
+                    message: Internal server error
+                - code: 200
+                    message: Success
+        """
+        try:
+            serializer = self.serializer_class(data=request.data)
+            if serializer.is_valid():
+                
+                data = serializer.data
+                try:
+                    user = User.objects.get(email= data['email'])
+               
+                    if user.password == data['password']:
+                        return Response({'message': "User successfully logged in"}, status=status.HTTP_200_OK)
+                except:
+                    return Response("Invalid details", status=status.HTTP_401_UNAUTHORIZED)
+
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except (AttributeError, KeyError, TypeError) as error:
+            logging.error(error, exc_info=True)
+            content = {'message': "INTERNAL_SERVER_ERROR"}
+            return Response(content, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+
+
+
+
+
+
+
+
+
