e2f024c4eeb80fd07d2be633627503c09bc69f1f
Yash <yashmahes@gmail.com>
2018-11-30 12:09:50 +0530

    created serializers and models

--- mysite/User/serializers.py
+++ mysite/User/serializers.py
@@ -1,7 +1,15 @@
 from rest_framework import serializers
-
+from .models import User
 # class UserRegisterSerializer(serializers.Hyper):
 #     pass
+class UserRegisterSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('name', 'password', 'email', 'role')
 
+class UserLoginSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('password', 'email')
 
 
--- mysite/User/views.py
+++ mysite/User/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets
+from .serializers import UserLoginSerializer, UserRegisterSerializer
+from .models import User
+
+
+
